df16beb5baf7c5b16373dd3039494b91b32c511d
Kostas Karachalios <vrinek@me.com>
2013-07-07 19:10:35 +0200

    Refactor input processor and allow for alternative card input syntax

---
+++ input_processor.rb
@@ -0,0 +1,143 @@
+class InputProcessor
+	CARD_NUMBER_RX = /^(\d+)( +(-?\d+))?( *f)?$/
+	HELP_TEXT = <<-TEXT
+		Enter a set code to select that set.
+			Examples:
+				"dmg", for "Dragon's Maze"
+				"m13", for "Magic 2013"
+		Enter a number to add a card to your collection.
+			After the number, you can specify the quantity and whether it's a foil.
+			Syntax is:
+				"<number> (<quantity>) (f)"
+			Examples:
+				"13 2", number 13, quantity 2
+				"13 f", number 13, foil (quantity is 1 by default)
+				"13f", same as above
+				"13 5 f", number 13, quantity 5, foil
+				"13 -1", number 13, quantity -1 (removes 1 card from the collection)
+		Enter nothing to exit the program.
+		Other commands:
+			"sets": displays the list of all known sets
+			"help/?": displays this help text
+			"cards": displays current card collection
+	TEXT
+
+	attr_reader :set_code
+
+	def initialize(verbose = true)
+		@verbose = verbose
+	end
+
+	def process_input(input)
+		if input.empty? # end of story
+			end_processing
+		elsif input == "?" || input == "help" # display help
+			display_help
+		elsif input == "cards" # display cards collection
+			display_card_collection
+		elsif input =~ /^cards \w+$/
+			set_code = input[/^cards (\w+)$/, 1]
+			display_cards_for set_code
+		elsif input == "sets" # list of known sets
+			display_known_sets
+		elsif SETS.has_key?(input.split[0]) # change set
+			change_set input.split[0]
+			rest_of_input = input.sub(/^\w+ /, '')
+			if rest_of_input != input && rest_of_input =~ CARD_NUMBER_RX
+				add_card rest_of_input
+			end
+			input
+		elsif input =~ CARD_NUMBER_RX # add/remove card to collection
+			add_card input
+		else
+			puts "Uknown command. Type \"?\" for help."
+		end
+	end
+
+	private
+
+	def end_processing
+		return false
+	end
+
+	def display_help
+		puts HELP_TEXT
+	end
+
+	def display_card_collection
+		puts "Current collection:"
+		$cards.each do |set_code, card_numbers|
+			puts SETS[set_code] || "foils"
+			rarities = Hash.new(0)
+			set_code = set_code.sub(/ foil$/, '')
+			cards_list = cards_list_for(set_code)
+			card_numbers.each do |(number, quantity)|
+				card = find_card(number, cards_list)
+				rarities[card[:rarity]] += 1
+			end
+			rarities.each do |rarity, quantity|
+				puts "%4d %s"%[quantity, rarity]
+			end
+		end
+	end
+
+	def display_cards_for(set_code)
+		set_name = SETS[set_code]
+		puts "Cards in #{set_name}:"
+		cards_list = cards_list_for(set_code)
+		$cards[set_code].each do |(number, quantity)|
+			card = find_card(number, cards_list)
+			puts "%3d x %3d - %s (%s)"%[quantity, number, card[:name], card[:rarity]]
+		end
+		if $cards[set_code + " foil"]
+			puts "Foil cards in #{set_name}:"
+			$cards[set_code + " foil"].each do |(number, quantity)|
+				card = find_card(number, cards_list)
+				puts "%3d x %3d - %s (%s)"%[quantity, number, card[:name], card[:rarity]]
+			end
+		end
+	end
+
+	def display_known_sets
+		puts "Known sets:"
+		ap SETS
+	end
+
+	def change_set(input)
+		@set_code = input
+		@cards_list = Checklist.new(@set_code).tap(&:fetch!).cards
+		return input
+	end
+
+	def add_card(input)
+		number, _, amount, foil = input.scan(CARD_NUMBER_RX)[0]
+		amount ||= 1
+		set_code = foil ? @set_code + " foil" : @set_code
+		$cards[set_code] ||= Hash.new(0)
+		$cards[set_code][number.to_i] += amount.to_i
+		if $cards[set_code][number.to_i] == 0
+			$cards[set_code].delete number.to_i
+		elsif $cards[set_code][number.to_i] < 0
+			raise "The card collection cannot contain negative quantities of cards"
+		end
+		if @verbose
+			card = find_card(number, @cards_list)
+			ap card
+			next_ten = @cards_list.select do |card|
+				card[:number] > number.to_i && card[:number] <= number.to_i + 10
+			end
+			next_ten.sort_by { |card| card[:number] }.each do |card|
+				puts "%3d - %s"%[card[:number], card[:name]]
+			end
+		end
+		return input
+	end
+
+	def find_card(number, cards_list)
+		cards_list.find { |card| card[:number] == number.to_i }
+	end
+
+	def cards_list_for(set_code)
+		Checklist.new(set_code.sub(/ foil$/, '')).tap(&:fetch!).cards
+	end
+end
--- mtg.rb
+++ mtg.rb
@@ -1,127 +1,26 @@
 require "awesome_print"
 require "./sets.rb"
 require "./checklist.rb"
+require "./input_processor.rb"
 require 'open-uri'
 
 $cards = {}
-HELP_TEXT = <<-TEXT
-	Enter a set code to select that set.
-		Examples:
-			"dmg", for "Dragon's Maze"
-			"m13", for "Magic 2013"
-	Enter a number to add a card to your collection.
-		After the number, you can specify the quantity and whether it's a foil.
-		Syntax is:
-			"<number> (<quantity>) (f)"
-		Examples:
-			"13 2", number 13, quantity 2
-			"13 f", number 13, foil (quantity is 1 by default)
-			"13f", same as above
-			"13 5 f", number 13, quantity 5, foil
-			"13 -1", number 13, quantity -1 (removes 1 card from the collection)
-	Enter nothing to exit the program.
-	Other commands:
-		"sets": displays the list of all known sets
-		"help/?": displays this help text
-		"cards": displays current card collection
-TEXT
-CARD_NUMBER_RX = /^(\d+)( +(-?\d+))?( *f)?$/
 
-def find_card(number, cards_list)
-	cards_list.find { |card| card[:number] == number.to_i }
-end
-
-def cards_list_for(set_code)
-	Checklist.new(set_code.sub(/ foil$/, '')).tap(&:fetch!).cards
-end
-
-# @return String, nil, false
-# 	`String` to log
-# 	`nil` to not log
-# 	`false` to exit
-def process_input(input, verbose = true)
-	if input.empty? # end of story
-		return false
-	elsif input == "?" || input == "help" # display help
-		puts HELP_TEXT
-	elsif input == "cards" # display cards collection
-		puts "Current collection:"
-		$cards.each do |set_code, card_numbers|
-			puts SETS[set_code] || "foils"
-			rarities = Hash.new(0)
-			set_code = set_code.sub(/ foil$/, '')
-			cards_list = cards_list_for(set_code)
-			card_numbers.each do |(number, quantity)|
-				card = find_card(number, cards_list)
-				rarities[card[:rarity]] += 1
-			end
-			rarities.each do |rarity, quantity|
-				puts "%4d %s"%[quantity, rarity]
-			end
-		end
-	elsif input =~ /^cards \w+$/
-		set_code = input[/^cards (\w+)$/, 1]
-		set_name = SETS[set_code]
-		puts "Cards in #{set_name}:"
-		cards_list = cards_list_for(set_code)
-		$cards[set_code].each do |(number, quantity)|
-			card = find_card(number, cards_list)
-			puts "%3d x %3d - %s (%s)"%[quantity, number, card[:name], card[:rarity]]
-		end
-		puts "Foil cards in #{set_name}:"
-		$cards[set_code + " foil"].each do |(number, quantity)|
-			card = find_card(number, cards_list)
-			puts "%3d x %3d - %s (%s)"%[quantity, number, card[:name], card[:rarity]]
-		end
-	elsif input == "sets" # list of known sets
-		puts "Known sets:"
-		ap SETS
-		return nil
-	elsif SETS.has_key?(input) # change set
-		@set_code = input
-		@cards_list = Checklist.new(@set_code).tap(&:fetch!).cards
-		return input
-	elsif input =~ CARD_NUMBER_RX # add/remove card to collection
-		number, _, amount, foil = input.scan(CARD_NUMBER_RX)[0]
-		amount ||= 1
-		set_code = foil ? @set_code + " foil" : @set_code
-		$cards[set_code] ||= Hash.new(0)
-		$cards[set_code][number.to_i] += amount.to_i
-		if $cards[set_code][number.to_i] == 0
-			$cards[set_code].delete number.to_i
-		elsif $cards[set_code][number.to_i] < 0
-			raise "The card collection cannot contain negative quantities of cards"
-		end
-		if verbose
-			card = find_card(number, @cards_list)
-			ap card
-			next_ten = @cards_list.select do |card|
-				card[:number] > number.to_i && card[:number] <= number.to_i + 10
-			end
-			next_ten.sort_by { |card| card[:number] }.each do |card|
-				puts "%3d - %s"%[card[:number], card[:name]]
-			end
-		end
-		return input
-	else
-		puts "Uknown command. Type \"?\" for help."
-		return nil
-	end
-end
-
+processor = InputProcessor.new(false)
 LOG_FILE = "cards.log"
 if File.exists?(LOG_FILE)
 	File.open(LOG_FILE, 'r') do |cards_file|
 		cards_file.each_line do |line|
-			process_input line.strip, false
+			processor.process_input line.strip
 		end
 	end
 end
 
+processor = InputProcessor.new
 File.open(LOG_FILE, 'a') do |cards_file|
 	loop do
-		print "set: #{SETS[@set_code.to_s]} > "
-		result = process_input gets.strip
+		print "set: #{SETS[processor.set_code] || "NONE"} > "
+		result = processor.process_input gets.strip
 		if result == false
 			break
 		elsif result.is_a?(String)
