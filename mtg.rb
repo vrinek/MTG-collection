b1f51a6993ccd7e5d4e48e05917feb7f5bc8dc64
Kostas Karachalios <vrinek@me.com>
2013-07-22 20:13:43 +0200

    Better variable names

--- mtg.rb
+++ mtg.rb
@@ -7,24 +7,24 @@ require 'open-uri'
 $cards = {}
 
 processor = InputProcessor.new(false)
-LOG_FILE = "cards.log"
-if File.exists?(LOG_FILE)
-	File.open(LOG_FILE, 'r') do |cards_file|
-		cards_file.each_line do |line|
+LOG_FILENAME = "cards.log"
+if File.exists?(LOG_FILENAME)
+	File.open(LOG_FILENAME, 'r') do |log_file|
+		log_file.each_line do |line|
 			processor.process_input line.strip
 		end
 	end
 end
 
 processor = InputProcessor.new
-File.open(LOG_FILE, 'a') do |cards_file|
+File.open(LOG_FILENAME, 'a') do |log_file|
 	loop do
 		print "set: #{SETS[processor.set_code] || "NONE"} > "
 		result = processor.process_input gets.strip
 		if result == false
 			break
 		elsif result.is_a?(String)
-			cards_file.puts result
+			log_file.puts result
 		end
 	end
 end
